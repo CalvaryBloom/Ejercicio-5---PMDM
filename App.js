b1194a8f8532be5dae14d74c4d7e1d590aa7feb7
CalvaryBloom <borjap99@gmail.com>
2025-10-30 10:46:39 +0100

    Apartado 4

--- App.js
+++ App.js
@@ -3,36 +3,53 @@
 * AUTOR: BORJA PARDO JUANES
 * FECHA: 30/10/2025 
 *
-* Apartado 3:
-* Modifica el ejercicio anterior para que ahora se guarden en un array los
-* valores numéricos y en otro el resto de valores y ambos arrays se
-* rendericen debajo del componente button. Para el array correspondiente a
-* las letras se debe mostrar cada posición en un componente Text. Lo mismo
-* para el array que contiene los valores numéricos. 
+* Apartado 4:
+* Modifica el ejercicio anterior para que ahora se guarden en un array sólo las
+* letras introducidas mediante TextInput. Implementa debajo del
+* componente Button otros dos componentes TextInput y Button.
+* Implementa la lógica necesaria para que después de haber introducido las
+* posiciones del array pertinente, se muestre por pantalla el contenido de la
+* posición del array que se indique con el segundo TextInput. Nota: en caso
+* de intentar acceder a una posición del array que no existe, saltará una
+* alerta. 
 */
 import { useState } from 'react';
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [indice, setIndice] = useState();
   const [texto, setTexto] = useState('');
-  const [myArray, setMyArray] = useState([]);
+  const [texto2, setTexto2] = useState('');
   const [myTextArray, setMyTextArray] = useState([]);
 
   const handleOnPress = () => {
     if (texto === '') {
       alert('No has introducido nada.');
     } else if (!isNaN(texto)) {
-      alert('Tú número se ha guardado.');
-      let newArray = [...myArray];
-      newArray.push(texto);
-      setMyArray(newArray);
+      alert('Solo se puede introducir texto.');
       setTexto('');
     } else {
       alert('Tú texto se ha guardado.');
-      let newArray = [...myArray];
+      let newArray = [...myTextArray];
       newArray.push(texto);
       setMyTextArray(newArray);
       setTexto('');
+    }
+  }
+
+  const mostrarIndiceArray = () => {
+    if (indice === ''){
+      alert('No has introducido nada.');
+      setIndice('');
+    } else if (isNaN(indice)){
+      alert('Solo puedes introducir un número');
+      setIndice('');
+    }else if (parseInt(indice) > myTextArray.length - 1 || parseInt(indice) < 0){
+      alert('Introduce una posición correcta.');
+      setIndice('');
+    }else{
+      setIndice(parseInt(indice));
+      setTexto2(myTextArray[indice]);
     }
   }
 
@@ -49,13 +66,18 @@ export default function App() {
         <Text style={styles.text}>PULSA...</Text>
       </Pressable>
 
-      {myTextArray.map((value, index) => (
-        <Text key={index.toString()}>{value}</Text>
-      ))}
+      <TextInput
+        style={{ height: 40, marginBottom: 20, marginTop: 20, }}
+        placeholder="Inserta posición array ..."
+        onChangeText={(newText) => setIndice(newText)}
+        defaultValue= {indice}
+      />
 
-      {myArray.map((value, index) => (
-        <Text key={index.toString()}>{value}</Text>
-      ))}
+      <Pressable onPress={mostrarIndiceArray}>
+        <Text style={styles.text}>PULSA...</Text>
+      </Pressable>
+
+      <Text style={{ padding: 10, fontSize: 42 }}>{texto2}</Text>
 
     </View>
   );
