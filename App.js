9208f7c50a79325cb1f30652d8862e9cc62fac71
CalvaryBloom <borjap99@gmail.com>
2025-10-30 18:08:13 +0100

    Apartado 5

--- App.js
+++ App.js
@@ -3,82 +3,76 @@
 * AUTOR: BORJA PARDO JUANES
 * FECHA: 30/10/2025 
 *
-* Apartado 4:
-* Modifica el ejercicio anterior para que ahora se guarden en un array sólo las
-* letras introducidas mediante TextInput. Implementa debajo del
-* componente Button otros dos componentes TextInput y Button.
-* Implementa la lógica necesaria para que después de haber introducido las
-* posiciones del array pertinente, se muestre por pantalla el contenido de la
-* posición del array que se indique con el segundo TextInput. Nota: en caso
-* de intentar acceder a una posición del array que no existe, saltará una
-* alerta. 
+* Apartado 5:
+* En este ejercicio vamos a trabajar con arrays de objetos. Implementa ahora
+* dos componentes TextInput y un componente Button que permitan
+* introducir un nombre y un DNI –que deberá ser validado- en un array de
+* objetos. Debajo del componente button deberá mostrarse los datos que el
+* usuario va introduciendo. 
 */
 import { useState } from 'react';
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [indice, setIndice] = useState();
-  const [texto, setTexto] = useState('');
-  const [texto2, setTexto2] = useState('');
-  const [myTextArray, setMyTextArray] = useState([]);
+  const [textDni, setTextDni] = useState('');
+  const [textName, setTextName] = useState('');
+  const [user, setUser] = useState([{ nombre: 'Nombre', dni: 'Dni' }]);
 
   const handleOnPress = () => {
-    if (texto === '') {
-      alert('No has introducido nada.');
-    } else if (!isNaN(texto)) {
-      alert('Solo se puede introducir texto.');
-      setTexto('');
+    if (validateDni(textDni)) {
+      alert('DNI y nombre introducidos correctamente');
+      let newArray = [...user];
+      newArray.push({ nombre: textName, dni: textDni });
+      setUser(newArray);
+      setTextName('');
+      setTextDni('');
     } else {
-      alert('Tú texto se ha guardado.');
-      let newArray = [...myTextArray];
-      newArray.push(texto);
-      setMyTextArray(newArray);
-      setTexto('');
+      alert('Introduce un DNI y nombre correcto');
+      setTextDni('');
+      setTextName('');
     }
-  }
+  };
 
-  const mostrarIndiceArray = () => {
-    if (indice === ''){
-      alert('No has introducido nada.');
-      setIndice('');
-    } else if (isNaN(indice)){
-      alert('Solo puedes introducir un número');
-      setIndice('');
-    }else if (parseInt(indice) > myTextArray.length - 1 || parseInt(indice) < 0){
-      alert('Introduce una posición correcta.');
-      setIndice('');
-    }else{
-      setIndice(parseInt(indice));
-      setTexto2(myTextArray[indice]);
+  const validateDni = (textDni) => {
+    if (
+      textDni !== '' &&
+      textDni.length === 9 &&
+      isNaN(parseInt(textDni[textDni.length - 1])) &&
+      !isNaN(textDni.substring(0, 8)) &&
+      textName !== ''
+    ) {
+      return true;
+    } else {
+      return false;
     }
-  }
+  };
+
+  const users = user.map((value) => (
+    <Text>{value.nombre + ' / ' + value.dni}</Text>
+  ));
 
   return (
     <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>DNI</Text>
       <TextInput
-        style={{ height: 40, marginBottom: 100, }}
-        placeholder="Inserta tu texto ..."
-        onChangeText={(newText) => setTexto(newText)}
-        defaultValue={texto}
+        style={{ height: 40 }}
+        placeholder="Inserta DNI"
+        onChangeText={(newText) => setTextDni(newText)}
+        defaultValue={textDni}
+        value={textDni}
       />
-
+      <Text style={{ fontSize: 30 }}>Nombre</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Inserta nombre"
+        onChangeText={(newName) => setTextName(newName)}
+        defaultValue={textName}
+        value={textName}
+      />
       <Pressable onPress={handleOnPress}>
-        <Text style={styles.text}>PULSA...</Text>
+        <Text style={styles.text}>Pulsa...</Text>
       </Pressable>
-
-      <TextInput
-        style={{ height: 40, marginBottom: 20, marginTop: 20, }}
-        placeholder="Inserta posición array ..."
-        onChangeText={(newText) => setIndice(newText)}
-        defaultValue= {indice}
-      />
-
-      <Pressable onPress={mostrarIndiceArray}>
-        <Text style={styles.text}>PULSA...</Text>
-      </Pressable>
-
-      <Text style={{ padding: 10, fontSize: 42 }}>{texto2}</Text>
-
+      {users}
     </View>
   );
 }
